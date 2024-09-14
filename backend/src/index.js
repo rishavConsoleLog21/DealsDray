12bce6df3c5b540563070619679058e2dbce52b4
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-09-14 18:35:42 +0530

    Setup Express, CORS & connectDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
-    path: './.env'
+  path: "./.env",
 });
 
-connectDB();
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is running on port: ${process.env.PORT || 5000}`);
+    });
+    app.on("Error", (error) => {
+      console.log("Error While connecting MongoDB", error);
+      throw error;
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Error", error);
+  });
