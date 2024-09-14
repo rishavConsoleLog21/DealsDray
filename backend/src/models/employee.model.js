31376e332b371a4a81e0e644bf3d98d6f6933a5c
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-09-14 21:27:03 +0530

    Add User & Employee Model

---
+++ backend/src/models/employee.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+import paginate from "mongoose-paginate-v2";
+
+const empolyeeSchema = Schema(
+  {
+    name: { type: String, required: [true, "Name is required"] },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    phone: {
+      type: Number,
+      required: [true, "Phone number is required"],
+      unique: [true, "Phone number already exists"],
+    },
+    designation: { type: String, required: [true, "Designation is required"] },
+    gender: { type: String, required: [true, "Gender is required"] },
+    course: { type: String, required: [true, "Course is required"] },
+    image: { type: String, required: [true, "Image is required"] },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+empolyeeSchema.plugin(paginate);
+
+export const Employee = mongoose.model("Employee", empolyeeSchema);
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,72 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      trim: true,
+      minlength: [3, "Username must be at least 3 characters."],
+      maxlength: [20, "Username must be at most 20 characters."],
+      index: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters."],
+    },
+    avatar: {
+      type: String,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.verifyPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRES }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRES }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
