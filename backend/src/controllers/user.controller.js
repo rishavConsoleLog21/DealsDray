44c3c988880644b2d3509b2cb5518bec607b831b
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-09-15 02:13:48 +0530

    Add registerUser Controller & Bug Fix

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,8 +1,53 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
+import uploadOnCloudinary from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "ok" });
+  const { username, email, password } = req.body;
+
+  if ([username, email, password].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "Please provide all required fields");
+  }
+
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User already exists with that username or email");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Please provide an avatar image");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  // if (!avatar) {
+  //   throw new ApiError(500, "Failed to upload avatar image")}
+
+  const user = await User.create({
+    username: username.toLowerCase(),
+    email,
+    password,
+    avatar: avatar.url || "",
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Failed to create user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, createdUser, "User created"));
 });
 
 export { registerUser };
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  //NOTE: Change single to field is not working
+  upload.single({
+    name: "avatar",
+    maxCount: 1,
+  }),
+  registerUser
+);
 
 export default router;
