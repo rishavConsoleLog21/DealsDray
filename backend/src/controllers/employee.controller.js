ce61ff0810bf7ee5cbac4f4bad3a785ccaea34aa
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-09-16 02:30:26 +0530

    Add newEmployee

---
+++ backend/src/controllers/employee.controller.js
@@ -0,0 +1,73 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import uploadOnCloudinary from "../utils/cloudinary.js";
+import { Employee } from "../models/employee.model.js";
+
+// Routes to save/create a new employee
+const newEmployee = asyncHandler(async (req, res) => {
+  const { name, email, phone, designation, gender, course } = req.body;
+
+  try {
+    if (
+      [name, email, phone, designation, gender, course].some(
+        (field) => field?.trim() === ""
+      )
+    ) {
+      throw new ApiError(400, "Please provide all required fields");
+    }
+
+    if (phone.length !== 10) {
+      throw new ApiError(400, "Phone number should be 10 digits");
+    }
+
+    // Check if employee with the same email or phone number already exists
+    const existingEmployee = await Employee.findOne({
+      $or: [{ email }, { phone }],
+    });
+
+    if (existingEmployee) {
+      throw new ApiError(
+        409,
+        "Employee already exists with that email/phone number"
+      );
+    }
+
+    const imageLocalPath = req.files?.image[0]?.path;
+
+    if (!imageLocalPath) {
+      throw new ApiError(400, "Please provide an image");
+    }
+
+    const image = await uploadOnCloudinary(imageLocalPath);
+
+    if (!image) {
+      throw new ApiError(500, "Failed to upload image");
+    }
+
+    const employee = await Employee.create({
+      image: image.url,
+      name,
+      email,
+      phone,
+      designation,
+      gender,
+      course,
+    });
+
+    const createdEmployee = await Employee.findById(employee._id);
+
+    if (!createdEmployee) {
+      throw new ApiError(500, "Failed to create employee");
+    }
+
+    return res
+      .status(201)
+      .json(new ApiResponse(201, createdEmployee, "Employee created"));
+  } catch (error) {
+    console.log(error.message);
+    throw new ApiError(500, "Catch :: Failed to create employee");
+  }
+});
+
+export { newEmployee };
